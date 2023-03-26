2dd133af319ce951b3062ec29d7cb7cea9715fef
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-03-26 16:10:02 +0300

    Done With Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("#create-task-form")
+  form.addEventListener('submit',(e) =>{
+    e.preventDefault()
+    buildToDo(e.target.querySelector("#new-task-description").value)
+    form.reset()
+  })
 });
+
+function buildToDo(todo){
+  console.log(todo)
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener("click",handleDelete)
+  btn.textContent = 'X'
+  p.textContent =  `${todo} `
+  p.appendChild(btn)
+  console.log(p)
+  
+  document.querySelector('#list').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
+ 
